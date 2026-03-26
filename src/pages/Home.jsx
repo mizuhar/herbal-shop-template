b46d136b22939ef09af0d8baffa81f3fdc27efb6
mizuhar <mizuhar@abv.bg>
2026-03-26 07:45:42 +0200

    home css

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,22 @@ export default function Home() {
   return(
 
   <>
+    
+     
+     <section className="hero">
+  <div className="hero-content">
     <h1>Natural Herbal Products 🌿</h1>
-    <p>Discover the best herbs for your health and wellbeing.</p>
+
+    <p>
+      Discover premium herbs, teas and oils for your health and wellbeing.
+    </p>
+
+    <a href="/shop" className="hero-btn">
+      Shop Now
+    </a>
+  </div>
+</section>
+<h2 style={{ marginTop: "40px" }}>Featured Products</h2>
   </>
   );
 }
